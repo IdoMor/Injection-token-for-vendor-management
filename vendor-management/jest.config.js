653c67a2e442b98578a0abb0b866bc2127f652b4
Ido Mor <imor@yotpo.com>
2021-10-11 12:34:56 +0300

    add shared library

--- vendor-management/jest.config.js
+++ vendor-management/jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  projects: ['<rootDir>/apps/vendor-management-using-injection-tokens'],
+  projects: [
+    '<rootDir>/apps/vendor-management-using-injection-tokens',
+    '<rootDir>/libs/shared-lib',
+  ],
 };
